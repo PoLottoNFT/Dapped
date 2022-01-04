8dbca4135bf820bd74da75cb4f1e8e0eea84aca7
PoLottoNFT <97129270+PoLottoNFT@users.noreply.github.com>
2022-01-04 16:12:32 -0500

    Add files via upload

---
+++ public_html/precache-manifest.a88420869bb7ea575397787a70be6469.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cde22dfae05a163018cd",
+    "url": "/static/css/main.65911420.chunk.css"
+  },
+  {
+    "revision": "cde22dfae05a163018cd",
+    "url": "/static/js/main.cde22dfa.chunk.js"
+  },
+  {
+    "revision": "6a81c37ecd16eae3b3c1",
+    "url": "/static/js/1.6a81c37e.chunk.js"
+  },
+  {
+    "revision": "2888119372e165e2e645",
+    "url": "/static/js/2.28881193.chunk.js"
+  },
+  {
+    "revision": "3d5ce47e2e27f64569a9",
+    "url": "/static/js/runtime~main.3d5ce47e.js"
+  },
+  {
+    "revision": "e63562614cbdc491e4d374f97c513b82",
+    "url": "/static/media/font.e6356261.woff"
+  },
+  {
+    "revision": "1fa1ca4f14a668531bdbe60bb1b6f887",
+    "url": "/index.html"
+  }
+];
